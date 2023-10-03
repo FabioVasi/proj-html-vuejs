08d2e233f8cbd2a63a240adffc4b715b5c6ed54d
FabioVasi <vasifabio96@gmail.com>
2023-10-03 14:30:06 +0200

    end header start main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            name: 'AppMain'
+        }
+    }
+}
+</script>
+
+<template>
+
+    <main id="main_app">
+
+    </main>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
