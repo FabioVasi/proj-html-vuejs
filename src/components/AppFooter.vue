b0fd01383107cd4b75bb07d4c3a009a8da6c389c
FabioVasi <vasifabio96@gmail.com>
2023-10-04 10:32:06 +0200

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,33 +12,107 @@ export default {
 
     <footer id="footer_app">
 
-        <div class="d-flex justify-content-between align-items-center">
-    
+        <div class="d-flex justify-content-between align-items-start">
             <div class="col-2">
-                <div class="card border-0 bg-transparent ps-5 d-flex justify-content-between align-items-center">
+                <div class="card border-0 bg-transparent d-flex justify-content-center align-items-center pe-5">
                     <img width="150px" src="../assets/img/author-logo-round.png" alt="">
                 </div>
             </div>
             <div class="col-10">
-                <div class="row">
-                    <div class="col 4">
-                        
-                    </div>
-                    <div class="col 4">
-                        
-                    </div>
-                    <div class="col 4">
-                        
+                <div class="container">
+                    <div class="row g-5">
+                        <div class="col-4">
+                            <div class="card text-white border-0 bg-transparent pe-3">
+                                <h4 class="m-0 pb-4">ABOUT ME</h4>
+                                <p style="color: #c6c4c4;" class="m-0">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae ab veritatis iure quaerat fuga rem libero sequi! Animi ratione at eligendi, facere, iste similique ab eveniet facilis distinctio, iure ad.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card text-white border-0 bg-transparent">
+                                <h4 class="m-0 pb-4">USEFUL LINKS</h4>
+                                <ul class="list-group list-group-flush border-0 bg-transparent">
+                                    <li class="list-group-item border-0 bg-transparent">
+                                        <a class="text-white text-decoration-none" href="#">
+                                            Latest Books
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item border-0 bg-transparent">
+                                        <a class="text-white text-decoration-none" href="#">
+                                            Upcoming Events
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item border-0 bg-transparent">
+                                        <a class="text-white text-decoration-none" href="#">
+                                            Recent Articles
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item border-0 bg-transparent">
+                                        <a class="text-white text-decoration-none" href="#">
+                                            Business Enquiries
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item border-0 bg-transparent">
+                                        <a class="text-white text-decoration-none" href="#">
+                                            Visit My Foundation
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card text-white border-0 bg-transparent">
+                                <h4 class="m-0 pb-4">CONTACT INFO</h4>
+                                <ul class="list-group list-group-flush border-0 bg-transparent">
+                                    <li class="list-group-item contact border-0 bg-transparent">
+                                        63 E. Studebaker Streen Bolingbrook, IL 60440
+                                    </li>
+                                    <li class="list-group-item contact border-0 bg-transparent">
+                                        Phone: (800) 000-0000
+                                    </li>
+                                    <li class="list-group-item contact border-0 bg-transparent">
+                                        Mobile: (800) 000-0000
+                                    </li>
+                                    <li class="list-group-item  text-white border-0 bg-transparent">
+                                        <a class="text-white text-decoration-none" href="#">
+                                            Email: info@your-company.com
+                                        </a>
+                                    </li>
+                                                        
+                                </ul>
+                                <div class="pt-4">
+                                    <a href="#">
+                                        <svg class="icon pe-3 ps-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>
+                                    </a>
+                                    <a href="#">
+                                        <svg class="icon pe-3 ps-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
+                                    </a>
+                                    <a href="#">
+                                        <svg class="icon pe-3 ps-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                                    </a>
+                                    <a href="#">
+                                        <svg class="icon pe-3 ps-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg>
+                                    </a>
+                                    <a href="#">
+                                        <svg class="icon pe-3 ps-3" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-                    
+          </div>            
         </div>
 
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="card border-0 bg-transparent">
+                    <div class="card copyright pt-5 border-0 bg-transparent text-center">
+                        <p class="m-0">
+                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM199.4 312.6c-31.2-31.2-31.2-81.9 0-113.1s81.9-31.2 113.1 0c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9c-50-50-131-50-181 0s-50 131 0 181s131 50 181 0c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0c-31.2 31.2-81.9 31.2-113.1 0z"/></svg>
+                            Copyright 2012 - 2020 | Avada Theme by <a class="text-white text-decoration-none" href="#">Theme Fusion</a> | All Rights Reserved | Power by <a class="text-white text-decoration-none" href="#">WorldPress</a>
+                        </p>
                         
                     </div>
                 </div>
@@ -49,8 +123,6 @@ export default {
 
 </template>
 
-
-
 <style lang="scss" scoped>
     @use '../assets/scss/partials/variables.scss' as *;
 
@@ -59,5 +131,16 @@ export default {
         background-size: cover;
         padding-top: 100px;
         padding-bottom: 50px;
+    }
+    .contact{
+        color: #c6c4c4;
+    }
+    .icon{
+        fill: #c6c4c4;
+        font-size: 20px;
+
+    }
+    .copyright{
+        color: #c6c4c4;
     }
 </style>
